aca8dbc8db7615b8ea6a83053afca523baddda86
nadinegawad <nadinerifaat00@gmail.com>
2024-09-05 11:54:19 -0700

    Add project files.

---
+++ MVC-02/Controllers/HomeController.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace MVC_02.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult About()
+        {
+            return View();
+        }
+        public IActionResult privacy()
+        {
+            return View();
+        }
+        public IActionResult contact()
+        {
+            return View();
+        }
+        public IActionResult login()
+        {
+            return View();
+        }
+        public IActionResult register()
+        {
+            return View();
+        }
+
+    }
+}
---
+++ MVC-02/Controllers/MoviesController.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Net.Sockets;
+
+namespace MVC_02.Controllers
+{
+    public class MoviesController : Controller
+    {
+        public IActionResult GetMovie(int id,string name , Employee employee)
+        {
+            return Content($"Movie is id :{id}");
+        }
+        //public string Index()
+        //{
+        //    return "Hello From The Index";
+        //}
+        //public ContentResult Index()
+        //{
+        //    ContentResult result = new ContentResult() { Content = "Hello From The Index" };
+        //    return result;
+        //    //result.ContentType= "text/html";
+        //    //result.ContentType = "object/pdf";
+        //}
+
+        public IActionResult Index()
+        {
+           
+            return Content("Hello From The Index");
+            //result.ContentType= "text/html";
+            //result.ContentType = "object/pdf";
+        }
+        public IActionResult Redirect()
+        {
+            //return RedirectToAction(nameof(Index));
+            return RedirectToRoute(new { Controller = "Movies", action = "Index" });
+        }
+    }
+}
